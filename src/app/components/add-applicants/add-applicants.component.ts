5007497b7b97a3fd7b4c54cc830bb111c7072c87
Miguel <sorth29@gmail.com>
2020-11-16 04:59:32 -0600

    subida de archivos al cienazo full hd kt y yo

--- src/app/components/add-applicants/add-applicants.component.ts
+++ src/app/components/add-applicants/add-applicants.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, Form } from '@angular/forms';
+import { ApplicantsService } from '../../services/applicants.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-applicants',
@@ -7,25 +9,85 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./add-applicants.component.scss']
 })
 export class AddApplicantsComponent implements OnInit {
-   
-  formAspirante: FormGroup
+
+  formAspirante: FormGroup;
+  @ViewChild("carta", {
+    read: ElementRef
+  }) carta: ElementRef;
+
+  @ViewChild("c_postulacion", {
+    read: ElementRef
+  }) c_postulacion: ElementRef;
+
+  @ViewChild("justificacion", {
+    read: ElementRef
+  }) justificacion: ElementRef;
+
+  @ViewChild("estructura", {
+    read: ElementRef
+  }) estructura: ElementRef;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private applicants: ApplicantsService,
+    private cd: ChangeDetectorRef
   ) {
     this.formAspirante = this.formBuilder.group({
      nombre_propuesta: ['', [Validators.required, Validators.maxLength(30)]],
      p_aspirante: ['', Validators.required],
      s_aspirante: [''],
-     t_aspirante: [''],
-     carta: ['', Validators.required],
-     c_postulacion: ['', Validators.required],
-     alineacion: ['', Validators.required],
-     propuesta: ['', Validators.required]
+     t_aspirante: ['']
     });
   }
 
   ngOnInit(): void {
   }
+
+  onSubmit(files: FileList) {
+    let archivo = this.carta.nativeElement.files[0];
+    let archivo2 = this.c_postulacion.nativeElement.files[0];
+    let archivo3 = this.justificacion.nativeElement.files[0];
+    let archivo4 = this.estructura.nativeElement.files[0];
+
+    let id = JSON.parse(localStorage.getItem('session-data'));
+    let valores: any = new FormData();
+    // let headers = new Headers();
+    // headers.append('Content-Type','multipart/form-data');
+    valores.append("id_usuarios", id.id_usuarios);
+    console.log(id.id_usuarios+ 'usuario');
+    valores.append("nombre_propuesta", this.formAspirante.value.nombre_propuesta);
+    valores.append("p_aspirante", this.formAspirante.value.p_aspirante);
+    valores.append("s_aspirante", this.formAspirante.value.s_aspirante);
+    valores.append("t_aspirante", this.formAspirante.value.t_aspirante);
+    valores.append("carta", archivo);
+    valores.append("c_postulacion", archivo2);
+    valores.append("alineacion", archivo3);
+    valores.append("propuesta", archivo4);
+
+    this.applicants.registroPropuesta(valores).subscribe(
+      data => {
+        swal.fire({
+          icon: 'success',
+          title: 'Registro exitoso'
+        });
+        console.log(data);
+        
+      },
+      err => {
+        
+        swal.fire({
+          icon: 'warning',
+          title: 'Ha ocurrido un error al registrar',
+          text: 'Favor de revisar que todos los campos se encuentren llenos y el peso de los archivos sea el'
+        });
+        console.log(err);
+        
+      }
+
+    );  
+    
+  }
+
+
 
 }
