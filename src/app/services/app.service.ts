b31055211c2e37dd9f7a070d5ba085404999d51a
RaulHdzDev <rahdz.0799@gmail.com>
2020-12-04 22:47:10 -0600

    tiempoTerminado

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class AppService {
 
   constructor() { }
-  API_URL = 'http://mante.hosting.acm.org/api-premio-estatal-2020/public/';
-  // API_URL = 'http://plataforma.cotacyt.gob.mx/api-premio-estatal-2020/public/';
+  //API_URL = 'http://mante.hosting.acm.org/api-premio-estatal-2020/public/';
+  API_URL = 'http://plataforma.cotacyt.gob.mx/api-premio-estatal-2020/public/';
   API_CP_URL = 'https://api-sepomex.hckdrk.mx/query/info_cp/';
 }
